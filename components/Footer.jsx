f773f350d709c571602ab74895ad3ddd166bb062
Ayo <heyayomideadebisi@gmail.com>
2023-04-22 11:44:59 +0100

    add footer copyright

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,10 @@
+import {AiOutlineCopyright} from "react-icons/ai"
 
 const Footer = () => {
   return (
     <footer className=" max-w-[72rem] mx-auto border-t border-t-neutral-800 p-2">
       <section className="">
-        <h6 className="text-sm text-neutral-700">2023 Ayomide.</h6>
+        <h6 className="text-sm text-neutral-700 flex items-center "><AiOutlineCopyright/>2023 Ayomide.</h6>
       </section>
     </footer>
   );
--- components/Header.jsx
+++ components/Header.jsx
@@ -5,24 +5,29 @@ import gitsvg1 from "@/assets/icons8-github (1).svg";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import {AiOutlineDown} from "react-icons/ai"
+import {IoIosArrowForward} from "react-icons/io"
+import {MdArrowForwardIos} from "react-icons/md"
 
 const Header = () => {
   const [open, setOpen] = useState(false);
   const openMenu = () => {
-    setOpen(!open);
+    setOpen(!open);         
   };
 
   return (
     <div className=" flex flex-col sm:flex-row sm:justify-between items-center p-2">
       <div className="flex justify-between items-center w-full">
-        <h2 className="font-bold text-lg sm:text-2xl text-white">Ayo.Web</h2>
+        <h2 className="font-bold text-2xl sm:text-2xl text-white">Ayo.Web</h2>
         <div>
-          <ul className="sm:hidden cursor-pointer text-green-600" onClick={openMenu}>
+          <ul className="sm:hidden font-medium cursor-pointer text-green-600 flex items-center gap-2" onClick={openMenu}>
+            <MdArrowForwardIos className={!open && "hidden"}/>
+            <AiOutlineDown className={open && "hidden"}/>
             <li>MENU</li>
           </ul>
         </div>
       </div>
-      <ul className={open ? "hidden sm:flex" : "flex w-full justify-end space-x-6 mt-2 sm:mt-0  "}>
+      <ul className={open ? "hidden sm:flex" : "flex justify-end w-full text-sm items-end gap-6 mt-4 sm:mt-0 "}>
         <li className="text-md hover:text-green-700 ">
           <Link href="/">/Home</Link>
         </li>
