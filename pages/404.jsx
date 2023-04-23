6d8bad39c90278e4401f6e95c1508572c5bf23ff
Ayo <heyayomideadebisi@gmail.com>
2023-04-23 15:20:04 +0100

    add 404 page details

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,16 +1,22 @@
-import Footer from "@/components/Footer"
-
+import Footer from "@/components/Footer";
+import Link from "next/link";
 
 export default function Custom404() {
   return (
-    <div className="px-4 bg-black text-[#999] py-1 min-h-screen">
-        <section className="max-w-[60rem] mx-auto mt-4">
+    <div className=" bg-black text-[#999] h-screen">
+      <section className="max-w-[60rem] mx-auto mt-4 px-4">
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h2 className=" text-lg md:text-xl">Hi there, this page you are trying to access is not available.</h2>
+          <p className=" text-lg md:text-xl">But don&apos;t worry, Lets us take you home.</p>
 
-        Not found
-        </section>
-    <section className="fixed bottom-0 w-full backdrop-blur-2xl">
+          <Link href="/">
+            <span className=" underline text-2xl font-bold md:text-xl">Home</span>
+          </Link>
+        </div>
+      </section>
+      <section className="fixed bottom-0 w-full backdrop-blur-2xl">
         <Footer />
       </section>
     </div>
-  )
+  );
 }
